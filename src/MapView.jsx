214a0c5e43faa97fd8a42e82cc9a2a0e2c6d9da1
Ben <benlandry56@gmail.com>
2025-08-11 06:34:35 -0600

    Fix build issues: correct data import and Leaflet icon paths - Fix named export import for destinations data and use CDN URLs for Leaflet icons to resolve Vite build errors

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -8,14 +8,14 @@ import {
   ZoomControl
 } from "react-leaflet";
 import L from "leaflet";
-import stops from "./data";
+import { destinations as stops } from "./data";
 import "./MapView.css";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl:      require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl:    require("leaflet/dist/images/marker-shadow.png"),
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
 export default function MapView() {
